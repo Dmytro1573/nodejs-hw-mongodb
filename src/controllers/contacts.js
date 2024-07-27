cf78f68f1f77f180964aeba2baac9f109fb25017
Dmytro1573 <124176235+Dmytro1573@users.noreply.github.com>
2024-07-27 14:53:02 +0200

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -86,6 +86,10 @@ async function changeEmailController(req, res, next) {
 
   const changedContact = await changeContactEmail(contactId, contact);
 
+  if (!changedContact) {
+    return next(createHttpError(404, 'Contact not found'));
+  }
+
   res.status(200).send({
     status: 200,
     message: 'Successfully patched a contact!',
